68e9879ff419b36a8335882a9e2904c260868bf6
Guilherme Assi <guiassi@hotmail.com>
2023-07-11 21:18:13 -0300

    feat: failure registry application test

--- SideDesk.ClientRegister/SideDesk.ClientRegister.Test/RegistryApplication/RegistryApplicationRegistry.cs
+++ SideDesk.ClientRegister/SideDesk.ClientRegister.Test/RegistryApplication/RegistryApplicationRegistry.cs
@@ -24,11 +24,11 @@ namespace SideDesk.ClientRegister.Test
 
 		[Fact(DisplayName = "Success")]
 		[Trait("Application", "Registry")]
-		public void Registry_ReturnSuccess() 
+		public void Registry_ReturnSuccess()
 		{
 			//Arrange
 			var request = _fixture.Create<RegistryRequest>();
-			var client = _fixture.Build<Client>().With(a => a.Name, request.Name).With(a=> a.Document, request.Document).Create();
+			var client = _fixture.Build<Client>().With(a => a.Name, request.Name).With(a => a.Document, request.Document).Create();
 			var registryResponse = _fixture.Build<RegistryResponse>().With(a => a.Name, client.Name).With(a => a.Document, client.Document).Create();
 
 			_clientRepository.Setup(a => a.SaveChanges()).Returns(1);
@@ -41,6 +41,27 @@ namespace SideDesk.ClientRegister.Test
 			//Assert
 			Assert.True(response.Success);
 			Assert.Equal(response.Model, registryResponse);
+		}
+
+		[Fact(DisplayName = "Failure")]
+		[Trait("Application", "Registry")]
+		public void Registry_ReturnFalse()
+		{
+			//Arrange
+			var request = _fixture.Build<RegistryRequest>().With(a=> a.Name,"Test").With(a=> a.Document, "123.123.123-12").Create();
+			var client = _fixture.Build<Client>().With(a => a.Name, request.Name).With(a => a.Document, request.Document).Create();
+			var registryResponse = _fixture.Build<RegistryResponse>().With(a => a.Name, client.Name).With(a => a.Document, client.Document).Create();
+
+			_clientRepository.Setup(a => a.SaveChanges()).Returns(0);
+			_mapper.Setup(a => a.Map<Client>(request)).Returns(client);
+			_mapper.Setup(a => a.Map<RegistryResponse>(client)).Returns(registryResponse);
+
+			//Act
+			var response = _registryApplication.Registry(request);
+
+			//Assert
+			Assert.False(response.Success);
+			Assert.Equal("Fail to registry client with document 123.123.123-12, try again later!", response.Messages.First());
 		}
 	}
 }
