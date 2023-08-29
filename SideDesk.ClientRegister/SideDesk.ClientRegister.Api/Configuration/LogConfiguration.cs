f3954ae931eee9b716be86cbaa3ee0b6c62488a2
Guilherme Assi <guiassi@hotmail.com>
2023-08-29 19:27:46 -0300

    feat: serilog.

---
+++ SideDesk.ClientRegister/SideDesk.ClientRegister.Api/Configuration/LogConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Serilog;
+using SideDesk.ClientRegister.Infrastructure.Helpers;
+
+namespace SideDesk.ClientRegister.Api.Configuration
+{
+	public static class LogConfiguration
+	{
+		public static IServiceCollection ConfigureLogger(this IServiceCollection services)
+		{
+			LogConfigurator.CreateLog();
+			return services.AddLogging(logginBuilder => logginBuilder.AddSerilog(dispose: true));
+		}
+	}
+}
